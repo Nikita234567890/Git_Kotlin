b67c09ac53bb2ce32e2d1f141c56316e89b390d5
Nikita <nik.bagnuk@mail.ru>
2022-11-06 13:14:10 +0300

    added task 2

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,6 @@
 fun main(args: Array<String>) {
     doTask1()
+    println(doTask2( 5))
 }
 fun doTask1(){
     val a = readLine()?.toIntOrNull()
@@ -14,4 +15,18 @@ fun doTask1(){
             println("even ")
         }
     }
+}
+fun doTask2(data: Int): Int{
+    if (data < 0)
+        return -1
+
+    if (data ==0)
+        return 1
+
+    var res = 1
+    for (i in 1 .. data)
+    {
+        res  *= i
+    }
+    return res
 }
